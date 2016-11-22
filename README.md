14c05a065db1050eeded4c7e70a54b6a20afaf90
brianswisher <bswisher@gmail.com>
2016-11-22 07:29:01 -0800

    init commit

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # tdd-react-enzyme-article
-
-A repo for the [A TDD approach on testing React components using Enzyme](http://thereignn.ghost.io/a-step-by-step-tdd-approach-on-testing-react-components-using-enzyme/) I wrote.
-
-### Usage
-
-1. Clone the repo
-2. Run `npm install`
-3. Run `npm test`
--- tests.js
+++ tests.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
-import sinon from 'sinon';
-import nock from 'nock';
-import request from 'superagent';
-
-
+import React from 'react'
+import nock from 'nock'
+import sinon from 'sinon'
+import request from 'superagent'
+import { expect } from 'chai'
+import { shallow, mount, render } from 'enzyme'
 
 const UserComponent = (props) => {
   return (
@@ -13,52 +11,16 @@ const UserComponent = (props) => {
       <p className="user__name">Name: { props.name }</p>
       <p className="user__age">Age: { props.age }</p>
     </div>
-  );
-};
-
-
-
-let wrapper;
-
-
-
-describe('Test suite for UserComponent', () => {
-  beforeEach(() => {
-    // Prevent duplication
-    wrapper = shallow(<UserComponent
-                            name={ 'Reign' }
-                            age={ 26 } />);
-  });
-
-  it('UserComponent should exist', () => {
-    expect(wrapper).to.exist;
-  });
-
-  it('Correctly displays the user name and age in paragraphs wrapped under a parent div', () => {
-    expect(wrapper.type()).to.equal('div');
-    expect(wrapper.hasClass('user')).to.equal(true);
-
-    let namePar = wrapper.childAt(0);
-    expect(namePar.type()).to.equal('p');
-    expect(namePar.hasClass('user__name')).to.equal(true);
-    expect(namePar.text()).to.equal('Name: Reign');
-
-    let agePar = wrapper.childAt(1);
-    expect(agePar.type()).to.equal('p');
-    expect(agePar.hasClass('user__age')).to.equal(true);
-    expect(agePar.text()).to.equal('Age: 26');
-  });
-});
-
-
+  )
+}
 
 class UsersListComponent extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       usersList: []
-    };
+    }
   }
 
   componentDidMount() {
@@ -66,26 +28,26 @@ class UsersListComponent extends React.Component {
       .get('https://api.github.com/users')
       .end((err, res) => {
         if (err) {
-          console.log(err);
-          return;
+          console.log(err)
+          return
         }
 
         this.setState({
           usersList: res.body.slice(0)
-        });
-      });
+        })
+      })
   }
 
   render() {
     if (!this.state.usersList.length) {
-      return null;
+      return null
     }
 
     return (
       <div className="users-list">
         { this._constructUsersList() }
       </div>
-    );
+    )
   }
 
   _constructUsersList() {
@@ -95,27 +57,54 @@ class UsersListComponent extends React.Component {
               key={ index }
               name={ user.name }
               age={ user.age } />
-      );
-    });
+      )
+    })
   }
 }
 
+let wrapper
 
+describe('Test suite for User component', () => {
+  beforeEach(() => {
+    // Prevent duplication
+    wrapper = shallow(<UserComponent
+                            name={ 'Reign' }
+                            age={ 26 } />)
+  })
+
+  it('UserComponent should exist', () => {
+    expect(wrapper).to.exist
+  })
+
+  it('Correctly displays the user name and age in paragraphs wrapped under a parent div', () => {
+    expect(wrapper.type()).to.equal('div')
+    expect(wrapper.hasClass('user')).to.equal(true)
+
+    let namePar = wrapper.childAt(0)
+    expect(namePar.type()).to.equal('p')
+    expect(namePar.hasClass('user__name')).to.equal(true)
+    expect(namePar.text()).to.equal('Name: Reign')
+
+    let agePar = wrapper.childAt(1)
+    expect(agePar.type()).to.equal('p')
+    expect(agePar.hasClass('user__age')).to.equal(true)
+    expect(agePar.text()).to.equal('Age: 26')
+  })
+})
 
 describe('Test suite for UsersListComponent', () => {
   beforeEach(() => {
     // Note the `beforeEach` from the start
-    wrapper = mount(<UsersListComponent />);
-  });
+    wrapper = mount(<UsersListComponent />)
+  })
 
   it('Renders null based on the initial state (empty `usersList` array)', () => {
-    expect(wrapper.state().usersList).to.be.instanceof(Array);
-    expect(wrapper.state().usersList.length).to.equal(0);
-    expect(wrapper.html()).to.equal(null);
-  });
+    expect(wrapper.state().usersList).to.be.instanceof(Array)
+    expect(wrapper.state().usersList.length).to.equal(0)
+    expect(wrapper.html()).to.equal(null)
+  })
 
-  it('Renders the root `div` with the right class and calls `_constructUsersList` to create the users list', () => {
-    sinon.spy(UsersListComponent.prototype, '_constructUsersList');
+  it('Renders the root `div` with the right class and renders correct children of type `User`', () => {
     wrapper.setState({
       usersList: [
         {
@@ -123,10 +112,35 @@ describe('Test suite for UsersListComponent', () => {
           age: 26
         }
       ]
-    });
+    })
+
+    expect(wrapper.state().usersList).to.be.instanceof(Array)
+    expect(wrapper.state().usersList.length).to.equal(1)
+    expect(wrapper.find('.users-list')).to.have.length(1)
+
+    let child = wrapper.childAt(0)
+    let childProps = child.props()
+
+    expect(child.type()).to.equal(UserComponent)
+    expect(childProps.name).to.equal('Reign')
+    expect(childProps.age).to.equal(26)
+  })
+
+  it('Renders the root `div` with the right class and calls `_constructUsersList` to create the users list', () => {
+    sinon.spy(UsersListComponent.prototype, '_constructUsersList')
+
+    wrapper.setState({
+      usersList: [
+        {
+          name: 'Reign',
+          age: 26
+        }
+      ]
+    })
+
     expect(wrapper.find('.users-list')).to.have.length(1);
     expect(UsersListComponent.prototype._constructUsersList.calledOnce).to.equal(true);
-  });
+  })
 
   it('The `_constructUsersList` behaves correctly', () => {
     wrapper.setState({
@@ -140,34 +154,59 @@ describe('Test suite for UsersListComponent', () => {
           age: 30
         }
       ]
-    });
-    const res = wrapper.instance()._constructUsersList();
-    expect(res).to.be.instanceof(Array);
-    expect(res.length).to.equal(2);
-    expect(mount(res[0]).type()).to.equal(UserComponent);
-    expect(res[0].props.name).to.equal('Reign');
-    expect(res[0].props.age).to.equal(26);
-    expect(mount(res[1]).type()).to.equal(UserComponent);
-    expect(res[1].props.name).to.equal('Vlad');
-    expect(res[1].props.age).to.equal(30);
-  });
+    })
+
+    const res = wrapper.instance()._constructUsersList()
+
+    expect(res).to.be.instanceof(Array)
+    expect(res.length).to.equal(2)
+    expect(mount(res[0]).type()).to.equal(UserComponent)
+    expect(res[0].props.name).to.equal('Reign')
+    expect(res[0].props.age).to.equal(26)
+    expect(mount(res[1]).type()).to.equal(UserComponent)
+    expect(res[1].props.name).to.equal('Vlad')
+    expect(res[1].props.age).to.equal(30)
+  })
 
   it('Correctly updates the state after AJAX call in `componentDidMount` was made', (done) => {
     nock('https://api.github.com')
       .get('/users')
       .reply(200, [
         { 'name': 'Reign', 'age': 26 }
-      ]);
+      ])
+
+    const oComponentDidMount = UsersListComponent.componentDidMount
+
+    UsersListComponent.componentDidMount = function () {
+      oComponentDidMount.apply(UsersListComponent)
+
+      console.info({
+        dump: 1234
+      })
+
+      // expect(wrapper.state().usersList).to.be.instanceof(Array)
+      // expect(wrapper.state().usersList.length).to.equal(1)
+      // expect(wrapper.state().usersList[0].name).to.equal('Reign')
+      // expect(wrapper.state().usersList[0].age).to.equal(26)
+      // nock.cleanAll()
+      // done()
+
+      // UsersListComponent.componentDidMount = oComponentDidMount
+    }
+
     // Overwrite, so we can correctly reason about the count number
     // Don't want shared state
-    wrapper = mount(<UsersListComponent />);
+    wrapper = mount(<UsersListComponent />)
+
+
+    // trying to avoid setTimeout
     setTimeout(function() {
-      expect(wrapper.state().usersList).to.be.instanceof(Array);
-      expect(wrapper.state().usersList.length).to.equal(1);
-      expect(wrapper.state().usersList[0].name).to.equal('Reign');
-      expect(wrapper.state().usersList[0].age).to.equal(26);
-      nock.cleanAll();
-      done();
-    }, 1500);
-  });
-});
+      expect(wrapper.state().usersList).to.be.instanceof(Array)
+      expect(wrapper.state().usersList.length).to.equal(1)
+      expect(wrapper.state().usersList[0].name).to.equal('Reign')
+      expect(wrapper.state().usersList[0].age).to.equal(26)
+      nock.cleanAll()
+      done()
+    }, 1500)
+  })
+})
